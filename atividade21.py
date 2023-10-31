08b116448ceb185d1e35a891199277b68790b2f0
gabriellevims <gabriellevi2104@gmail.com>
2023-10-31 18:23:05 -0300

    fogos

---
+++ atividade21.py
@@ -0,0 +1,9 @@
+# Exercício Python 21: Faça um programa que mostre na tela uma contagem regressiva para o estouro de
+# fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+import time
+contador = 10
+while (contador):
+     print("em", contador)
+     contador = contador - 1
+     time.sleep (1.0)
+print("explodiu🎇🎆🎇🧨🧨🧨🧨")
